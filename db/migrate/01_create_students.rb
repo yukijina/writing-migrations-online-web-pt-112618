b028fcae92a3bf8eccb41d8a7e810cac0f346eb6
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-19 00:30:36 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -9,7 +9,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def changed
     binding.pry
     create_table :students do |student|
-      inding.pry
       student.string :name
     end 
   end
