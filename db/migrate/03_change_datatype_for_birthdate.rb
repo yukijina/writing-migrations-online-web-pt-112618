7ded321a02b0e9f433d926b9544e609f0a3f3e27
Yukiji Nakatsuka <lunarbase@outlook.com>
2019-02-19 00:47:31 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthday < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change 
     change_column :students, :birthdate, :datetime
